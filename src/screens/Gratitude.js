70574e2de24808dab9067260aceab256db865690
Rajesh Kumar <cyber.geek205@gmail.com>
2019-10-09 18:29:43 +0530

    Internal Navigation Applied

---
+++ src/screens/Gratitude.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import styles from './style'
+export default class Gratitude extends React.Component {
+ //Screen1 Component
+ render() {
+    return (
+      <View style={styles.MainContainer}>
+        <Text style={{ fontSize: 23 }}> Graditude</Text>
+      </View>
+    );
+  }
+}
